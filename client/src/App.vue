b144c0754c6d5a127706fd59ca4caa9d252a2f82
tranthethang <tranthethang@gmail.com>
2023-07-19 00:13:59 +0700

    create form login

---
+++ client/src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import LoginForm from './components/LoginForm.vue'
+</script>
+
+<template>
+  <main>
+    <LoginForm></LoginForm>
+  </main>
+</template>
+
+<style scoped>
+</style>
---
+++ client/src/components/LoginForm.vue
@@ -0,0 +1,21 @@
+<script setup></script>
+
+<template>
+  <form>
+    <p>
+      <label>
+        <span>Email</span><br />
+        <input type="text" />
+      </label>
+    </p>
+    <p>
+      <label>
+        <span>Password</span><br />
+        <input type="text" />
+      </label>
+    </p>
+    <p>
+      <button type="submit">Login</button>
+    </p>
+  </form>
+</template>
