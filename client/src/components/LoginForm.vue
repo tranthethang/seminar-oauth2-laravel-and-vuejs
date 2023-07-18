f85f9651619f6fc92c67000a3dda359f9a1de984
tranthethang <tranthethang@gmail.com>
2023-07-19 00:23:54 +0700

    submit form

--- client/src/components/LoginForm.vue
+++ client/src/components/LoginForm.vue
@@ -5,17 +5,37 @@
     <p>
       <label>
         <span>Email</span><br />
-        <input type="text" />
+        <input type="text" v-model="email" />
       </label>
     </p>
     <p>
       <label>
         <span>Password</span><br />
-        <input type="text" />
+        <input type="text" v-model="password" />
       </label>
     </p>
     <p>
-      <button type="submit">Login</button>
+      <button type="submit" @click="submit">Login</button>
     </p>
   </form>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    submit() {
+      debugger
+      this.$emit("submit", {
+        email: this.email,
+        password: this.password,
+      });
+    },
+  },
+};
+</script>
