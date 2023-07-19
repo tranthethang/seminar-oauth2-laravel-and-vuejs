b8b216c7a653aaef50b6e74efff4373483a9191f
thangtt-codluck <thangtt@codluck.com>
2023-07-19 17:32:30 +0700

    install on ubuntu

--- api/app/Models/OauthAccessTokenAttributes.php
+++ api/app/Models/OauthAccessTokenAttributes.php
@@ -3,13 +3,13 @@
 use MennoVanHout\LaravelModelConstants\Types\ModelAttributes;
 class OauthAccessTokenAttributes extends ModelAttributes
 {
-	 const ID = 'id';
-	 const USER_ID = 'user_id';
 	 const CLIENT_ID = 'client_id';
-	 const NAME = 'name';
-	 const SCOPES = 'scopes';
-	 const REVOKED = 'revoked';
 	 const CREATED_AT = 'created_at';
-	 const UPDATED_AT = 'updated_at';
 	 const EXPIRES_AT = 'expires_at';
+	 const ID = 'id';
+	 const NAME = 'name';
+	 const REVOKED = 'revoked';
+	 const SCOPES = 'scopes';
+	 const UPDATED_AT = 'updated_at';
+	 const USER_ID = 'user_id';
 }
--- api/app/Models/OauthAuthCodeAttributes.php
+++ api/app/Models/OauthAuthCodeAttributes.php
@@ -3,10 +3,10 @@
 use MennoVanHout\LaravelModelConstants\Types\ModelAttributes;
 class OauthAuthCodeAttributes extends ModelAttributes
 {
-	 const ID = 'id';
-	 const USER_ID = 'user_id';
 	 const CLIENT_ID = 'client_id';
-	 const SCOPES = 'scopes';
-	 const REVOKED = 'revoked';
 	 const EXPIRES_AT = 'expires_at';
+	 const ID = 'id';
+	 const REVOKED = 'revoked';
+	 const SCOPES = 'scopes';
+	 const USER_ID = 'user_id';
 }
